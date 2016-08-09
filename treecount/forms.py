b89eb0345913fe8e29cea89db41c8ae80e95e46f
t00n <kelkununtel@hotmail.com>
2016-08-09 10:46:24 +0200

    Initial commit

---
+++ treecount/forms.py
@@ -0,0 +1,5 @@
+from models import Expense
+from django import forms
+
+class ExpenseAddForm(forms.Form):
+	pass
---
+++ treecount/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Expense(models.Model):
+	date = models.DateField(auto_now_add=True)
+	description = models.CharField(max_length=500)
+	amount = models.FloatField()
+	creditors = models.ManyToManyField(User, related_name='creditor')
+	debitors = models.ManyToManyField(User, related_name='debitor')
+
+	class Meta:
+		ordering = ('date',)
